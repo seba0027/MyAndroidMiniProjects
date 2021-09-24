685cc9e398c805db462f81395235aa07fe374e13
Jules Sebastian <seba0027@algonquinlive.com>
2021-09-24 01:18:03 -0400

    Worked on TextWidgets and CompoundWidgets.

--- app/src/main/java/algonquin/cst2335/seba0027/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/seba0027/MainActivity.java
@@ -2,7 +2,17 @@ package algonquin.cst2335.seba0027;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mytext = findViewById(R.id.textview);
+
+
+        EditText myedit = findViewById(R.id.myedittext);
+
+        Button btn = findViewById(R.id.mybutton);
+        if(btn != null)btn.setOnClickListener(v -> mytext.setText("Your edit text has: " + myedit.getText().toString()));
+
+        RadioButton radioB = findViewById(R.id.rb);
+
+        CheckBox checkB = findViewById(R.id.cb);
+        checkB.setOnCheckedChangeListener( (b, c) -> {
+            radioB.setChecked(c);
+            Toast.makeText(MainActivity.this, "You clicked on CheckBox", Toast.LENGTH_LONG).show();
+        } );
+
+        Switch switchButton = findViewById(R.id.sw);
+        switchButton.setOnCheckedChangeListener(  ( x , y ) -> {
+            radioB.setChecked(y);
+            Toast.makeText(MainActivity.this, "You clicked on Switch", Toast.LENGTH_SHORT).show();
+        } );
+
+        radioB.setOnCheckedChangeListener( ( s,t) -> {
+            Toast.makeText(MainActivity.this, "You clicked on RadioButton", Toast.LENGTH_SHORT).show();
+        });
+
+
+
+
+
     }
 }
